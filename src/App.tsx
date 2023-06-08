b84f1fdb9c212179a10bc35af0842875d784b877
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-08 16:22:56 -0500

    geolocate and select working

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,23 @@ import { API } from './lib/constants';
 import { convertToCustomDate } from './lib/helpers';
 
 function App() {
-  const [city, setCity] = useState<string>('33.75,-84.39');
+  const [city, setCity] = useState<string>('Atlanta');
+  const [cityIsSelected, toggleCityIsSelected] = useState<boolean>(false);
   const [weather, setWeather] = useState<Weather>();
   const [date, setDate] = useState<CustomDate | undefined>();
 
-  // ========== EXTRACT TO CUSTOM HOOK ========== //
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCity(e.target.value);
+    toggleCityIsSelected(true);
+  };
+
   useEffect(() => {
+    if (navigator.geolocation && !cityIsSelected) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setCity(`${position.coords.latitude},${position.coords.longitude}`);
+      });
+    }
+
     const fetchWeather = async () => {
       const { data } = await axios.get(
         `${API.BASE_URL}?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}`,
@@ -19,7 +30,7 @@ function App() {
       setDate(convertToCustomDate(data.location.localtime));
     };
     fetchWeather();
-  }, []);
+  }, [city, cityIsSelected]);
 
   return (
     <div>
@@ -36,7 +47,7 @@ function App() {
             {weather.location.name}, {weather.location.region}
           </h2>
           <h3>{weather.current.temp_f}°F</h3>
-          {/* replace with custom icons? */}
+          {/* replace with custom icons */}
           <img
             src={weather.current.condition.icon}
             alt={weather.current.condition.text}
@@ -44,6 +55,13 @@ function App() {
           <h3>{weather.current.condition.text}</h3>
         </>
       )}
+      <form>
+        <select onChange={handleChange}>
+          <option value='Atlanta'>Atlanta</option>
+          <option value='New York'>New York</option>
+          <option value='Los Angeles'>Los Angeles</option>
+        </select>
+      </form>
     </div>
   );
 }
