b83f5605fc19a690b16969d6cb921cc91c0505be
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-08 16:18:14 -0500

    weather to state

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { API } from './lib/constants';
 
 function App() {
   const [city, setCity] = useState<string>('Atlanta');
+  const [weather, setWeather] = useState<any>({});
 
   useEffect(() => {
     const fetchWeather = async () => {
       const { data } = await axios.get(
         `${API.BASE_URL}?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}`,
       );
-      console.log(data);
+      setWeather(data);
     };
     fetchWeather();
   }, []);
