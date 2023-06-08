873c0b26c7949396b30f8a3ee044ede8b96e22f3
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-08 16:17:13 -0500

    api call logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import { API } from './lib/constants';
+
 function App() {
-  return <div>test</div>;
+  const [city, setCity] = useState<string>('Atlanta');
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const { data } = await axios.get(
+        `${API.BASE_URL}?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}`,
+      );
+      console.log(data);
+    };
+    fetchWeather();
+  }, []);
+
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  );
 }
 
 export default App;
