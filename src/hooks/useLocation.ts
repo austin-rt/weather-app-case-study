d109609835ca18a96a20a0e65de909586f5c3322
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-10 08:29:19 -0500

    add IP as backup, '' = initial value, mv useGeolocation -> useLocation

---
+++ src/hooks/useLocation.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useAppDispatch } from '../store/store';
+import { setCity } from '../store/features/CitySlice';
+
+export default function useLocation() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        dispatch(setCity(`${latitude},${longitude}`));
+      });
+    } else {
+      const fetchIpLocation = async () => {
+        const { data }: { data: IpData } = await axios.get('http://ip-api.com/json/?fields=61439');
+        dispatch(setCity(`${data.lat},${data.lon}`));
+      };
+      fetchIpLocation();
+    }
+  }, []);
+}
