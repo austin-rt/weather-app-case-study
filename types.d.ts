a9833672c3007077a6c8adf6843055f1198ba7fb
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-10 08:30:52 -0500

    type ip data

--- types.d.ts
+++ types.d.ts
@@ -242,3 +242,20 @@ type CustomDate = {
   day: string;
   hour: string;
 };
+
+interface IpData {
+  status: string;
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+  query: string;
+}
