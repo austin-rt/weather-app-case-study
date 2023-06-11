ecd7067d861f8398b5fb6006137af5e903250335
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-11 06:48:50 -0500

    add animation inside dispatch

--- src/hooks/useFetchWeather.ts
+++ src/hooks/useFetchWeather.ts
@@ -3,8 +3,11 @@ import axios from 'axios';
 import { API } from '../lib/constants';
 import { setWeather } from '../store/features/WeatherSlice';
 import { useAppDispatch, useAppSelector } from '../store/store';
+import useLocation from './useLocation';
+import { addAnimationString } from '../lib/helpers';
 
 const useFetchWeather = () => {
+  useLocation();
   const city = useAppSelector(({ CitySlice }) => CitySlice.city);
   const dispatch = useAppDispatch();
 
@@ -14,7 +17,7 @@ const useFetchWeather = () => {
         const { data }: { data: Weather } = await axios.get(
           `${API.BASE_URL}/${API.CURRENT}?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}`,
         );
-        dispatch(setWeather(data));
+        dispatch(setWeather(addAnimationString(data)));
       }
     };
     fetchWeather();
