ad6819a124cafb2198f3bb4527ede863394be34a
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-12 07:47:10 -0400

    use long day string

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,7 +1,7 @@
 export const generateCustomDate = (): CustomDate => {
   const dateObj = new Date();
   return {
-    day: dateObj.toLocaleDateString('en-US', { weekday: 'short' }),
+    day: dateObj.toLocaleDateString('en-US', { weekday: 'long' }),
     hour: dateObj.toLocaleTimeString([], { hour: 'numeric' }),
   };
 };
