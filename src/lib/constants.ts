a7a4895ca48a1176d66072dacd13d22402a315bc
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-12 10:30:29 -0400

    change api const to enum

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
-export const API = {
-  BASE_URL: 'http://api.weatherapi.com/v1',
-  CURRENT: 'current.json',
-  SEARCH: 'search.json',
-};
+export enum API {
+  BASE_URL = 'http://api.weatherapi.com/v1',
+  CURRENT = 'current.json',
+  SEARCH = 'search.json',
+}
