68368e554cb0a64dc694e50cf642c43116354d2c
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-12 20:37:55 -0400

    refactor using store

--- src/hooks/useInputState.ts
+++ src/hooks/useInputState.ts
@@ -1,7 +1,17 @@
-import { useState } from 'react';
+import { useAppDispatch } from '../store/store';
+import { setDebouncedSearchQuery } from '../store/features/DebouncedSearchQuerySlice';
+import { useEffect, useState } from 'react';
+import useDebounce from './useDebounce';
 
 export default function useInputState() {
-  const [searchQuery, setSearchQuery] = useState<string>('');
+  const dispatch = useAppDispatch();
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const debouncedSearchQuery = useDebounce(searchQuery, 500) as string;
+
+  useEffect(() => {
+    dispatch(setDebouncedSearchQuery(debouncedSearchQuery));
+  }, [debouncedSearchQuery, dispatch]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
