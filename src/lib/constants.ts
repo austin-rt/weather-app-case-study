5d2f94bbca94bcc376dd9b1948d73ce55de7f5d5
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-13 14:03:13 -0400

    make animations record const instead of actual enum

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,25 +6,25 @@ export enum API {
   IP_FALLBACK = 'http://ip-api.com/json/?fields=61439',
 }
 
-export const enum ANIMATIONS {
-  CLOUDY_NIGHT = 'cloudyNight',
-  FOG = 'fog',
-  MIST = 'mist',
-  CLEAR_NIGHT = 'clearNight',
-  PARTLY_CLOUDY = 'partlyCloudy',
-  PARTLY_SHOWER = 'partlyShower',
-  RAIN = 'rain',
-  RAINY_NIGHT = 'rainyNight',
-  SNOW = 'snow',
-  SNOWY_SUN = 'snowySun',
-  SNOWY_NIGHT = 'snowyNight',
-  STORM = 'storm',
-  STORM_SHOWERS = 'stormShowers',
-  SUNNY = 'sunny',
-  THUNDER = 'thunder',
-  WINDY = 'windy',
-  LOADING = 'loading',
-}
+export const ANIMATIONS: Record<string, AnimationString> = {
+  CLOUDY_NIGHT: 'cloudyNight',
+  FOG: 'fog',
+  MIST: 'mist',
+  CLEAR_NIGHT: 'clearNight',
+  PARTLY_CLOUDY: 'partlyCloudy',
+  PARTLY_SHOWER: 'partlyShower',
+  RAIN: 'rain',
+  RAINY_NIGHT: 'rainyNight',
+  SNOW: 'snow',
+  SNOWY_SUN: 'snowySun',
+  SNOWY_NIGHT: 'snowyNight',
+  STORM: 'storm',
+  STORM_SHOWERS: 'stormShowers',
+  SUNNY: 'sunny',
+  THUNDER: 'thunder',
+  WINDY: 'windy',
+  LOADING: 'loading',
+};
 
 export const enum LOTTIE_CHILD {
   CURRENT_WEATHER = 'currentWeather',
--- types/animations.ts
+++
@@ -1,3 +0,0 @@
-import { ANIMATIONS } from '../src/lib/constants';
-
-export type AnimationString = ANIMATIONS;
