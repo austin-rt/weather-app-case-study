4d7e18c2af93c99fe3774f89bcf459abd06aa766
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-13 11:42:08 -0400

    fix IP fallback option

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -2,22 +2,34 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useAppDispatch } from '../store/store';
 import { setCity } from '../store/features/CitySlice';
+import { API } from '../lib/constants';
 
 export default function useLocation() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        dispatch(setCity({ lat: latitude, lon: longitude }));
-      });
-    } else {
-      const fetchIpLocation = async () => {
-        const { data }: { data: IpData } = await axios.get('http://ip-api.com/json/?fields=61439');
+    const successCallback = (position: GeolocationPosition) => {
+      const { latitude, longitude } = position.coords;
+      dispatch(setCity({ lat: latitude, lon: longitude }));
+    };
+
+    const errorCallback = (error: GeolocationPositionError) => {
+      if (error.code === error.PERMISSION_DENIED) {
+        fetchIpLocation();
+      }
+    };
+
+    const fetchIpLocation = async () => {
+      try {
+        const { data }: { data: IpData } = await axios.get(API.IP_FALLBACK);
         dispatch(setCity({ lat: data.lat, lon: data.lon }));
-      };
-      fetchIpLocation();
+      } catch (error) {
+        console.log('Error fetching IP location:', error);
+      }
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
     }
   }, [dispatch]);
 }
