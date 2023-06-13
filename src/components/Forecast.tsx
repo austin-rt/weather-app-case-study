e08e1f8adfebb86233b2b6bb4128293592dd6cb0
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-13 12:56:27 -0400

    extract fetch forecast logic

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,35 +1,17 @@
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../store/store';
-import { API, LOTTIE_CHILD } from '../lib/constants';
-import axios from 'axios';
-import { setForecast } from '../store/features/ForecastSlice';
-import { addAnimationStringToForecast } from '../lib/helpers';
+import { useAppSelector } from '../store/store';
+import { LOTTIE_CHILD } from '../lib/constants';
 import LottieWrapper from './LottieWrapper';
+import useFetchForecast from '../hooks/useFetchForecast';
+import { renderDay } from '../lib/helpers';
 
 type Props = {
   className?: string;
 };
 
 export default function Forecast({ className }: Props) {
-  const city = useAppSelector(({ CitySlice }) => CitySlice.city);
+  useFetchForecast();
   const forecast = useAppSelector(({ ForecastSlice }) => ForecastSlice.forecast);
-  const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (city) {
-      try {
-        const fetchForecast = async () => {
-          const { data }: { data: { forecast: Forecast } } = await axios.get(
-            `${API.BASE_URL}/${API.FORECAST}?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city.lat},${city.lon}&days=14&aqi=no&alerts=no`,
-          );
-          dispatch(setForecast(addAnimationStringToForecast(data.forecast)));
-        };
-        fetchForecast();
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  }, [dispatch, city]);
   return (
     <div className={className}>
       {forecast && (
@@ -39,9 +21,7 @@ export default function Forecast({ className }: Props) {
               key={day.date}
               className='flex w-5/12 flex-col items-center gap-2 rounded-3xl bg-slate-50 bg-opacity-10 p-3 px-6 shadow-2xl'
             >
-              <h2 className='text-center'>
-                {new Date(day.date + 'T00:00:00').toLocaleDateString('en-US', { weekday: 'short' })}
-              </h2>
+              <h2 className='text-center'>{renderDay(day.date)}</h2>
               <LottieWrapper
                 child={LOTTIE_CHILD.FORECAST}
                 day={day.day}
