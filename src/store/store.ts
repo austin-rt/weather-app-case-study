42f7a9547ff1feaff1564caa30595f2c8da7cc9b
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-12 20:37:32 -0400

    combine additional reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,10 +3,14 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import CitySlice from './features/CitySlice';
 import WeatherSlice from './features/WeatherSlice';
+import DebouncedSearchQuerySlice from './features/DebouncedSearchQuerySlice';
+import SearchResultsSlice from './features/SearchResultsSlice';
 
 const reducers = combineReducers({
   CitySlice,
   WeatherSlice,
+  DebouncedSearchQuerySlice,
+  SearchResultsSlice,
 });
 
 export const store = configureStore({
